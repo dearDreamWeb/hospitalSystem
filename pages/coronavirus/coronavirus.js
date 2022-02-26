38a9b2fcb20fc1248b29ee47efaddd2905dbde82
wxb <superwang9527@163.com>
2022-02-26 09:54:13 +0800

    feat:肺炎疫情统计完成

--- pages/coronavirus/coronavirus.js
+++ pages/coronavirus/coronavirus.js
@@ -2,15 +2,23 @@
 var app = getApp()
 Page({
   data: {
-    imgUrls: [
-      '../../images/slider_1.jpeg',
-      '../../images/slider_2.jpeg',
-      '../../images/slider_3.jpeg'
-    ],
-    indicatorDots: true,
-    autoplay: true,
-    interval: 5000,
-    duration: 1000
+    // 新闻列表数据
+    list: [],
+    // 国内疫情统计数据
+    statisticsData: [],
+    // 疫情提示
+    tipsArr: [],
+    // 全球疫情统计数据
+    globalStatisticsData: [],
+
+    dataObj: {
+      confirmedCount: '累计确诊人数',
+      curedCount: '治愈人数',
+      currentConfirmedCount: '现存确诊人数',
+      deadCount: '死亡人数',
+      seriousCount: '重症人数',
+      suspectedCount: '疑似感染人数',
+    },
   },
   //事件处理函数
   bindViewTap: function () {
@@ -19,20 +27,87 @@ Page({
     })
   },
   onLoad: function () {
-    var that = this;
+    const {
+      dataObj
+    } = this.data;
+    // 请求疫情数据
     wx.request({
-      url: 'https://lab.isaaclin.cn/nCoV/api/news',
+      url: 'https://lab.isaaclin.cn/nCoV/api/overall',
       method: 'GET',
-      header: {
-        'content-type': 'application/json;charset=UTF-8' // 默认值
+      success: (res) => {
+        console.log(res.data.results[0]);
+        /**
+         * confirmedCount 累计确诊人数
+         * curedCount 治愈人数
+         * currentConfirmedCount 现存确诊人数
+         * deadCount 死亡人数
+         * seriousCount 重症人数
+         * suspectedCount 疑似感染人数
+         * updateTime 更新时间
+         * globalStatistics 全球疫情数据
+         */
+        const {
+          confirmedCount,
+          curedCount,
+          currentConfirmedCount,
+          deadCount,
+          seriousCount,
+          suspectedCount,
+          updateTime,
+          globalStatistics,
+          note1,
+          note2,
+          note3,
+          remark1,
+          remark2,
+          remark3
+        } = res.data.results[0];
+        const statisticsDataArr = [{
+            text: dataObj.confirmedCount,
+            count: confirmedCount,
+          },
+          {
+            text: dataObj.curedCount,
+            count: curedCount,
+          },
+          {
+            text: dataObj.currentConfirmedCount,
+            count: currentConfirmedCount,
+          },
+          {
+            text: dataObj.deadCount,
+            count: deadCount,
+          },
+          {
+            text: dataObj.seriousCount,
+            count: seriousCount,
+          },
+          {
+            text: dataObj.suspectedCount,
+            count: suspectedCount,
+          },
+        ]
+        const tipsArr = [note1, note2, note3, remark1, remark2, remark3]
+        this.setData({
+          statisticsData: statisticsDataArr,
+          tipsArr
+        })
       },
-      success: function (res) {
+      fail() {
+        console.log('获取错误')
+      }
+    })
+    // 请求疫情新闻
+    wx.request({
+      url: 'https://lab.isaaclin.cn/nCoV/api/news?page=1&num=5',
+      method: 'GET',
+      success:  (res)=> {
         console.log(res.data.results);
         const data = res.data.results.map((item) => {
           item.pubDate = new Date(Number(item.pubDate)).toLocaleDateString();
           return item;
         })
-        that.setData({
+        this.setData({
           list: data,
         })
       },
